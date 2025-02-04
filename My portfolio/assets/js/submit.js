6ef1aa1a727904a69feb89775acb63d873ea0a3a
Jubilee <vanbawisung2007@gmail.com>
2025-02-04 22:17:44 +0630

    add portfolio website

---
+++ My portfolio/assets/js/submit.js
@@ -0,0 +1,7 @@
+// Submit button
+
+// let submit = document.getElementById('submit');
+document.getElementById("submit").onclick = function() {mySumit};
+function mySubmit(){
+  document.getElementById("paragraph").innerHTML = "Your form has been received. Thank you for contacting"
+}
